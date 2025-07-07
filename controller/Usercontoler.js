e5b5f0cff0c10f65b9a339e28a1d40fcc3ec0303
Md.Shimul Khan <shimulssk.121@gmail.com>
2025-07-07 23:13:19 +0600

    Update Usercontoler.js

--- controller/Usercontoler.js
+++ controller/Usercontoler.js
@@ -96,7 +96,7 @@ export const login = async (req, res) => {
 
         const token = await jwt.sign(tokenData, process.env.JWT_SECRECT_KEY, { expiresIn: '1d' });
 
-        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: ture, sameSite: 'strict' }).json({
+        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'strict' }).json({
             _id: user._id,
             username: user.username,
             fullName: user.fullName,
